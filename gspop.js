87302ea5d7baa71f65a679cefd772ac13ea40967
shabi1497 <54935409+shabi1497@users.noreply.github.com>
2019-09-16 11:25:59 +0530

    Add files via upload

---
+++ gspop.js
@@ -0,0 +1,56 @@
+; (function ($) {
+  var defaults =
+  {
+    position: "center centered", // 'top center', 'bottom center',
+    backdrop: {
+      show: true,
+      clickable: true
+    }
+  }
+
+  function gsPop(element, options) {
+    this.element = $(element);
+    this.options = $.extend({}, defaults, options);
+    this.init();
+    this.bindEvents();
+  }
+
+  gsPop.prototype.bindEvents = function () {
+    var $element = this.element;
+    $element.on('gspop.open', function () {
+      $('body').addClass('gs-body-settings');
+      $element.parent().show();
+    });
+
+    if(this.options.backdrop.clickable==true)
+    $element.next('.gs-Overlay').on('click', this.close.bind(this));
+    
+    $element.find('.gs-cancel').on('click', this.close.bind(this));
+
+    $element.on('gspop.close', function () {
+      $element.parent().hide();
+      $('body').removeClass('gspop-opened');
+    });
+    
+  }
+  gsPop.prototype.close = function(){
+    this.element.trigger('gspop.close');
+  }
+  gsPop.prototype.init = function () {
+    var $element = this.element;
+    $element.wrap("<div class='gs-Modal-Wrapper'></div>");
+    $element.addClass(this.options.position);
+    $(`<div class='gs-Overlay'></div>`).appendTo('.gs-Modal-Wrapper');
+
+    if(this.options.backdrop.show!=true)
+    $element.next().css('background','transparent');
+ 
+    $element.parent().hide();
+  }
+
+  $.fn.gsPop = function (options) {
+    return this.each(function (i, elm) {
+      new gsPop(elm, options);
+    });
+  }
+}(jQuery));
